2cbc136b91deb0527c0644f3d1b044802d45ee81
walks84 <daniel@hbplus.com.au>
2016-03-17 22:05:43 +1100

    NavFix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
-	$('.shopnowbutton, .addtocart, .signupbutton').hover(function() {
-		/* Stuff to do when the mouse enters the element */
-		$(this).css('background-color', 'black');
-	}, function() {
-		/* Stuff to do when the mouse leaves the element */
-		$(this).css('background-color', '#1abc9c');
-	});
+//	$('.shopnowbutton, .addtocart, .signupbutton').hover(function() {
+//		/* Stuff to do when the mouse enters the element */
+//		$(this).css('background-color', 'black');
+//	}, function() {
+//		/* Stuff to do when the mouse leaves the element */
+//		$(this).css('background-color', '#1abc9c');
+//	});
 
 	// $('.slider').hover(function() {
 	// 	/* Stuff to do when the mouse enters the element */
